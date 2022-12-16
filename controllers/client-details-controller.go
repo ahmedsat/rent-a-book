d925215a3fdb749f639f94a2c8920c44bc2f17dc
ahmedsat <ahmedsat303@gmail.com>
2022-12-16 09:25:50 +0000

    client details as json

--- controllers/client-details-controller.go
+++ controllers/client-details-controller.go
@@ -20,8 +20,32 @@ func ClientDetails(c *gin.Context) {
 		return
 	}
 
+	rentedBooksMap := make(map[uint]string)
+
+	for _, bookItem := range client.BookItems {
+
+		book := models.Book{}
+		result = database.Instance.Find(&book, bookItem.BookRefer)
+		if result.Error != nil {
+			c.HTML(http.StatusInternalServerError, "error.html", gin.H{
+				"error": result.Error.Error(),
+				"title": "error : 500",
+			})
+			return
+		}
+		rentedBooksMap[bookItem.BookRefer] = book.Title
+	}
+
+	// c.HTML(http.StatusOK, "client-details.html", gin.H{
+	// 	"title":  "book renter | client details",
+	// 	"client": client,
+	// 	"books":  rentedBooksMap,
+	// })
+
 	c.JSON(http.StatusOK, gin.H{
 		"title":  "book renter | client details",
 		"client": client,
+		"books":  rentedBooksMap,
 	})
+
 }
